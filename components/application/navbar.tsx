3f44897325a6398b80822929e3688aadccb1f71c
farukumarx64 <f.umaridris.mail@gmail.com>
2024-02-25 01:38:15 +0100

    user dropdown implemented

--- components/application/navbar.tsx
+++ components/application/navbar.tsx
@@ -1,29 +1,33 @@
 import {
-  Button,
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
   Avatar,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  Navbar as NextUINavbar,
+  DropdownSection,
+  User,
 } from "@nextui-org/react";
 
 import NextLink from "next/link";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 
-import Image from "next/image";
 import { useTheme } from "next-themes";
-import React, { useState } from "react";
+import Image from "next/image";
+import React from "react";
 
 interface NavbarProps {
   option: string;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({option}) => {
+export const Navbar: React.FC<NavbarProps> = ({ option }) => {
   const { theme, setTheme } = useTheme();
-  const [currentOption, setCurrentOption] = useState(option)
 
   return (
     <NextUINavbar maxWidth="xl" isBordered>
@@ -45,41 +49,49 @@ export const Navbar: React.FC<NavbarProps> = ({option}) => {
         </NavbarBrand>
         <NavbarItem>
           <NextLink href="/admin">
-          <Button
-            radius="full"
-            size="lg"
-            variant="light"
-            startContent={<i className="ri-stacked-view"></i>}
-            className={`flex items-center text-default-500 gap-2 ${option === 'Links' ? 'font-bold text-default-foreground' : ''}`}
-          >
-            <span className="">Links</span>
-          </Button>
+            <Button
+              radius="full"
+              size="lg"
+              variant="light"
+              startContent={<i className="ri-stacked-view"></i>}
+              className={`flex items-center text-default-500 gap-2 ${
+                option === "Links" ? "font-bold text-default-foreground" : ""
+              }`}
+            >
+              <span className="">Links</span>
+            </Button>
           </NextLink>
         </NavbarItem>
         <NavbarItem>
           <NextLink href="/admin/appearance">
-          <Button
-            radius="full"
-            size="lg"
-            variant="light"
-            startContent={<i className="ri-shapes-line"></i>}
-            className={`flex items-center text-default-500 gap-2 ${option === 'Appearance' ? 'font-bold text-default-foreground' : ''}`}
-          >
-            <span className="">Appearance</span>
-          </Button>
+            <Button
+              radius="full"
+              size="lg"
+              variant="light"
+              startContent={<i className="ri-shapes-line"></i>}
+              className={`flex items-center text-default-500 gap-2 ${
+                option === "Appearance"
+                  ? "font-bold text-default-foreground"
+                  : ""
+              }`}
+            >
+              <span className="">Appearance</span>
+            </Button>
           </NextLink>
         </NavbarItem>
         <NavbarItem>
           <NextLink href="/admin/settings">
-          <Button
-            radius="full"
-            size="lg"
-            variant="light"
-            startContent={<i className="ri-settings-line"></i>}
-            className={`flex items-center text-default-500 gap-2 ${option === 'Settings' ? 'font-bold text-default-foreground' : ''}`}
-          >
-            <span className="">Settings</span>
-          </Button>
+            <Button
+              radius="full"
+              size="lg"
+              variant="light"
+              startContent={<i className="ri-settings-line"></i>}
+              className={`flex items-center text-default-500 gap-2 ${
+                option === "Settings" ? "font-bold text-default-foreground" : ""
+              }`}
+            >
+              <span className="">Settings</span>
+            </Button>
           </NextLink>
         </NavbarItem>
       </NavbarContent>
@@ -100,7 +112,58 @@ export const Navbar: React.FC<NavbarProps> = ({option}) => {
           </Button>
         </NavbarItem>
         <NavbarItem className="hidden sm:flex gap-2">
-          <Avatar isBordered name="User" />
+          <Dropdown>
+            <DropdownTrigger>
+              <Avatar isBordered name="User" as="button" />
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Static Actions"
+              className="p-5"
+              disabledKeys={["user"]}
+              
+            >
+              <DropdownItem isReadOnly key="user" className="w-80 opacity-100">
+                <User
+                  name="@UserName"
+                  description="twgl.link/user"
+                  classNames={{
+                    name: "font-bold mb-1 ml-2",
+                    description: "text-default-500 ml-2",
+                  }}
+                  avatarProps={{
+                    size: "md",
+                    src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                  }}
+                />
+              </DropdownItem>
+              <DropdownSection title="Account" className="mt-4">
+                <DropdownItem
+                  key="my-account"
+                  className="w-80"
+                  startContent={<i className="ri-account-box-line text-xl"></i>}
+                >
+                  <span className="text-md ml-4">My account</span>
+                </DropdownItem>
+              </DropdownSection>
+              <DropdownSection title="Support" className="mt-4">
+                <DropdownItem
+                  key="submit-feedback"
+                  className="w-80"
+                  startContent={<i className="ri-feedback-line text-xl"></i>}
+                >
+                  <span className="text-md ml-4">Submit feedback</span>
+                </DropdownItem>
+              </DropdownSection>
+              <DropdownItem
+                key="delete"
+                className="text-danger w-80"
+                color="danger"
+                startContent={<i className="ri-logout-box-line text-xl"></i>}
+              >
+                <span className="text-md ml-4">Sign out </span>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
       </NavbarContent>
 
@@ -114,7 +177,21 @@ export const Navbar: React.FC<NavbarProps> = ({option}) => {
         >
           <span className="font-bold">Share</span>
         </Button>
-        <Avatar isBordered name="User" />
+        <Dropdown>
+          <DropdownTrigger>
+            <Button>
+              <Avatar isBordered name="User" />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownItem key="new">New file</DropdownItem>
+            <DropdownItem key="copy">Copy link</DropdownItem>
+            <DropdownItem key="edit">Edit file</DropdownItem>
+            <DropdownItem key="delete" className="text-danger" color="danger">
+              Delete file
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </NavbarContent>
 
       <NavbarMenu>
